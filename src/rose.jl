7edca0d59ebbd6d0349a016b6d3fa13f130dc672
Essam <essamwisam@outlook.com>
2023-07-17 19:15:11 +0300

    📝 Improve Documentation

--- src/rose.jl
+++ src/rose.jl
@@ -44,7 +44,7 @@ Oversample a dataset given by a matrix or table of observations X and a categori
 # Arguments
 
 - `X`: A matrix or table where each row is an observation (vector) of floats
-- `y::AbstractVector`: A categorical vector of labels that corresponds to the observations in X
+- `y`: A categorical array (vector) of labels that corresponds to the observations in X
 - `s::float`: A parameter that proportionally controls the bandwidth of the Gaussian kernel
 - `ratios`: A float or dictionary mapping each class to the ratio of the needed number of observations of that class to the current number of observations of the majority class. If nothing, then each class will be oversampled to the size of the majority class and if float then each class will be oversampled to the size of the majority class times the float.
 - `rng::AbstractRNG`: Random number generator
--- src/smote.jl
+++ src/smote.jl
@@ -88,7 +88,7 @@ Oversample a dataset given by a matrix or table of observations X and a categori
 
 # Arguments
 - `X`: A matrix or table where each row is an observation (vector) of floats
-- `y::AbstractVector`: A categorical vector of labels that corresponds to the observations in X
+- `y`: A categorical array (vector) of labels that corresponds to the observations in X
 - `k::Int`: Number of nearest neighbors to consider in the SMOTE algorithm. Should be within the range [1, numobs(X) - 1] else set to the nearest of these two values.
 - `ratios`: A float or dictionary mapping each class to the ratio of the needed number of observations of that class to the current number of observations of the majority class. If nothing, then each class will be oversampled to the size of the majority class and if float then each class will be oversampled to the size of the majority class times the float.
 - `rng::AbstractRNG`: Random number generator
