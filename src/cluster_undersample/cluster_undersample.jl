2432ea133b8e1a9569eaff090d38997061cb2b06
Essam <essamwisam@outlook.com>
2023-09-20 13:12:38 +0300

    improve cluster_undersample docs
    Co-authored-by: Anthony Blaom, PhD <anthony.blaom@gmail.com>

--- src/cluster_undersample/cluster_undersample.jl
+++ src/cluster_undersample/cluster_undersample.jl
@@ -54,8 +54,8 @@ $(COMMON_DOCS["INPUTS"])
 
 # Keyword Arguments
 
-- `mode::AbstractString="nearest`: If `center` then the undersampled data will consist of the centriods of 
-    each cluster found. Meanwhile, if `nearest` then it will consist of the nearest neighbor of each centroid.
+- `mode::AbstractString="nearest`: If `"center"` then the undersampled data will consist of the centriods of 
+    each cluster found; meanwhile, if `"nearest"` then it will consist of the nearest neighbor of each centroid.
 
 $(COMMON_DOCS["RATIOS-UNDERSAMPLE"])
 
