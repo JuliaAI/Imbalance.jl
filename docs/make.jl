85951940f79775f0855f19c267407eb8f2208d92
Essam <essamwisam@outlook.com>
2023-09-23 12:41:31 +0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -44,4 +44,4 @@ DocumenterTools.Themes.compile(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(repo = "github.com/JuliaAI/Imbalance.jl.git")
+deploydocs(repo = "github.com/JuliaAI/Imbalance.jl.git", devbranch = "dev")
