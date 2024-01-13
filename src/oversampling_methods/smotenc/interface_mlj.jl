179ee7fd0af180fa5b614f5039b06244f0b362ab
Essam <essamwisam@outlook.com>
2024-01-12 22:24:40 -0600

    🚑 Remove matrix support from MLJ docstring

--- src/oversampling_methods/smotenc/interface_mlj.jl
+++ src/oversampling_methods/smotenc/interface_mlj.jl
@@ -121,7 +121,7 @@ $((COMMON_DOCS["RNG"]))
 
 # Transform Inputs
 
-- `X`: A matrix of floats or a table with element [scitypes](https://juliaai.github.io/ScientificTypes.jl/) that subtype `Union{Finite, Infinite}`. 
+- `X`: A table with element [scitypes](https://juliaai.github.io/ScientificTypes.jl/) that subtype `Union{Finite, Infinite}`. 
      Elements in nominal columns should subtype `Finite` (i.e., have [scitype](https://juliaai.github.io/ScientificTypes.jl/) `OrderedFactor` or `Multiclass`) and
      elements in continuous columns should subtype `Infinite` (i.e., have [scitype](https://juliaai.github.io/ScientificTypes.jl/) `Count` or `Continuous`).
 
